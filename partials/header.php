7f003bece91465a62ff9bda09350e0f7968f8d90
livi83 <livia.kelebercova@gmail.com>
2024-02-28 16:18:22 +0100

    Fix

--- partials/header.php
+++ partials/header.php
@@ -6,6 +6,9 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/slider.css">
+    <link rel="stylesheet" href="css/portfolio.css">
+    <link rel="stylesheet" href="css/banner.css">
+    <link rel="stylesheet" href="css/accordion.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
