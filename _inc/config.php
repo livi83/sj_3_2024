a897f09e315e751016988ee6c1e43553e09641fc
Lívia Kelebercová <livia.kelebercova@gmail.com>
2024-03-20 14:27:05 +0100

    Uprava kontaktu, databaza

---
+++ _inc/config.php
@@ -0,0 +1,12 @@
+<?php
+
+//konštanta DATABASE obsahujúca asoc pole 
+define('DATABASE', [
+    'HOST' => 'localhost',
+    'DBNAME' => 'sj_3_2024',
+    'USER_NAME' => 'root',
+    'PASSWORD' => ''
+]);
+
+
+?>
--- templates/thankyou.php
+++ templates/thankyou.php
@@ -7,6 +7,36 @@ include('partials/header.php');
         <div class="row">
           <div class="col-100 text-center">
               <h2>Ďakujeme za vyplnenie formulára</h2>
+              <?php
+
+                //print_r($_POST);
+                 $conn = db_connection();
+                 if($conn){
+                  if(isset($_POST['contact_submitted'])){
+                      $data = array('contact_name'=>$_POST['name'],
+                      'contact_email'=>$_POST['email'],
+                      'contact_message'=>$_POST['message'],
+                      'contact_acceptance'=>$_POST['acceptance'],
+                    );
+
+                    try{
+
+                      $query = "INSERT INTO contact (name, email, message, acceptance) 
+                      VALUES (:contact_name, :contact_email, :contact_message, :contact_acceptance)";
+                      $query_run = $conn->prepare($query);
+                      $query_run->execute($data);
+
+                    }catch(PDOException $e){
+                     
+                      echo $e->getMessage();
+                    }
+
+                  }
+                 }else{
+                  echo 'Nemáme spojenie';
+                 } 
+
+              ?>
           </div>
         </div>
       </section>
