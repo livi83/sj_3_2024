3109bc51e9c8c47990fab6461d45100fec426d88
Lívia Kelebercová <livia.kelebercova@gmail.com>
2024-04-03 13:40:38 +0200

    get portfolio

--- _inc/classes/Portfolio.php
+++ _inc/classes/Portfolio.php
@@ -20,5 +20,32 @@
             }   
         }
 
+        function get_portfolio(){
+            $portfolio = $this->select();
+            $result = '';
+            for ($i=0;$i<count($portfolio);$i++){
+                $temp_i = $i+1;
+                if($temp_i%4==1){
+                    $result .= '<div class="row">';
+                    $result .= '<div class="col-25 portfolio text-white textcenter" style = "background-image: url(\''.$portfolio[$i]->image.'\');"'.'>';
+                    //$result .= '<a href = "">'.$portfolio[$i]->name.'</a>';
+                    $result .= '<a href="../templates/portfolio-single.php?id='.$portfolio[$i]->id.'">'.$portfolio[$i]->name.'</a>';
+                    $result .= '</div>';
+                }
+                else{
+                    $result .= '<div class="col-25 portfolio text-white textcenter" style = "background-image: url(\''.$portfolio[$i]->image.'\');"'.'>';
+                    //$result .= '<a href = "">'.$portfolio[$i]->name.'</a>';
+                    $result .= '<a href="../templates/portfolio-single.php?id='.$portfolio[$i]->id.'">'.$portfolio[$i]->name.'</a>';
+                    $result .= '</div>';
+                    $result .= '</div>';
+                }
+             
+            }
+            return $result;
+        }
+
+
+
+
     }
 ?>
