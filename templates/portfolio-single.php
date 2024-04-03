a9a273f912b7995ed2ffd58efc0fe3f7e50e7a78
Lívia Kelebercová <livia.kelebercova@gmail.com>
2024-04-03 14:03:01 +0200

    portfolio-single.php

--- templates/portfolio-single.php
+++ templates/portfolio-single.php
@@ -1,21 +1,22 @@
 <?php
-include('partials/header.php');
+include_once('partials/header.php');
 ?> 
-<main>
-      
-  <?php //include('partials/banner.php');?>
-      <section class="container">
-        <div class="row">
-          <div class="col-100 text-center">
-              <?php
+        <main>
+             
+              <section class="container">
+                <?php
+                    $portfolio_object = new Portfolio();
+                    $portfolio_single = $portfolio_object->select_single();
 
-                    
-              ?>
-          </div>
-        </div>
-      </section>
-    </main>
-    
+                    echo '<h2>'.$portfolio_single->name.'</h2>';
+                    echo '<img src="'.$portfolio_single->image.'" width="600"/>';
+                    echo '<br><br>';
+                    echo '<p>'.$portfolio_single->text.'</p>';
+              
+                ?>
+            </section>   
+
+        </main>
 <?php
-    include_once('partials/footer.php')
+  include_once('partials/footer.php')
 ?> 
