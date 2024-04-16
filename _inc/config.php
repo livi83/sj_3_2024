cf8243438fd1d06ba22fc7c8096b514459a6101c
Lívia Kelebercová <livia.kelebercova@gmail.com>
2024-04-16 20:43:32 +0200

    pridanie Slider class do configu

--- _inc/config.php
+++ _inc/config.php
@@ -14,6 +14,7 @@ require_once('classes/Database.php');
 require_once('classes/Contact.php');
 require_once('classes/Qna.php');
 require_once('classes/Portfolio.php');
+require_once('classes/Slider.php');
 require_once('classes/User.php');
 
 
